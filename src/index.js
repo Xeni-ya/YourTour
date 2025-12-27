c257639d02bf91d7a412ce62b10a26dd723ce493
Xeni-ya <budyuginaxeniya@gmail.com>
2025-12-27 04:11:25 +0300

    feat(*): configuring the build in package.json

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
-console.log("Hello This is a Javascript project.");
+// ================== Фиксированное меню ==================== //
+
+// появляется после 450px и меняет цвет
+
+const header = document.querySelector('.header');
+const logoBlack = document.querySelector('.icon use');
+const menuItems = document.querySelectorAll('.navbar__link');
+
+window.addEventListener('scroll', () => {
+  if (header) {
+    header.classList.toggle('scrolling', window.scrollY > 450);
+
+    // тогда:
+    // меняется логотип
+    if (logoBlack) {
+      if (window.scrollY > 450) {
+        logoBlack.setAttribute('xlink:href', './src/assets/icons/sprite.svg#logo-black');
+      } else {
+        logoBlack.setAttribute('xlink:href', './src/assets/icons/sprite.svg#logo-white');
+      }
+    }
+
+    // меняется цвет текста
+    if (menuItems.length > 0) {
+      if (window.scrollY > 450) {
+        menuItems.forEach(link => {
+          link.style.color = 'black'
+        });
+      } else {
+        menuItems.forEach(link => {
+          link.style.color = 'white';
+        });
+      }
+    }
+  }
+});
+
+
+// ================== Маска на номер ==================== //
+
+const inputField = document.getElementById('tel');
+
+const maskOptions = {
+  mask: '+7 (000) 000-00-00',
+  lazy: false,
+  placeholderChar: ' _ ',
+}
+
+// eslint-disable-next-line no-unused-vars
+const mask = window.IMask(inputField, maskOptions);
