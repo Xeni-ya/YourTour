3134246b556dddf7dd76e031f4fed6412f8ee60b
Xeni-ya <budyuginaxeniya@gmail.com>
2026-01-05 04:09:39 +0300

    feat(*): configuring html compilation + configuring style compilation

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+let mode = 'development';
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+}
+console.log(mode + ' mode ')
+
+export default {
+  mode: mode,
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  'postcss-preset-env',
+                ],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ]
+  },
+}
